675ca01cdea47e7b28f2a818b7ae16e6f94be3d7
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2024-02-05 16:52:24 +0900

    Edit profile

--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <main className="main">
-      <p>This is IT</p>
+      <p>name : Takumi OKAYASU</p>
+      <p>birthday : 1997/03/06</p>
+      <p>Language</p>
+      <ul>
+        <li>C++</li>
+        <li>Java</li>
+        <li>C#</li>
+        <li>VB.NET</li>
+        <li>TypeScript</li>
+      </ul>
     </main>
   );
 }
