1cd69bfeaecd9fea7620b7ec5db1ac00ab0f92da
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2024-02-28 10:51:18 +0900

    Add ExamplePage

--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -3,6 +3,7 @@ import style from "./page.module.css";
 export default function Home() {
   return (
     <main className={style.container}>
+      <a href="/example">こちら</a>
       <p>name : Takumi OKAYASU</p>
       <p>birthday : 1997/03/06</p>
       <p>Language</p>
